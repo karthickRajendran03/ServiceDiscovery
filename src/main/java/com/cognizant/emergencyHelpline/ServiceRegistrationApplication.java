9fd11c2899c073a870b7943959aeba258df391b3
KarthickRajendran03 <karthick.sacs@gmail.com>
2020-08-12 17:33:24 +0530

    initial release

---
+++ src/main/java/com/cognizant/emergencyHelpline/ServiceRegistrationApplication.java
@@ -0,0 +1,16 @@
+package com.cognizant.emergencyHelpline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistrationApplication.class, args);
+	}
+
+}
+
